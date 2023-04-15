c4b7eec2e059b66da19f30b97bc0248077ab0b43
Nils Langner <nils.langner@phmlabs.com>
2023-04-15 08:25:03 +0200

    Optimized CSV export example

--- example/retrieve-as-csv.php
+++ example/retrieve-as-csv.php
@@ -10,16 +10,23 @@ include_once __DIR__ . '/../vendor/autoload.php';
 
 $retriever = new Retriever(new Client());
 
-if (count($argv) !== 2) {
+if (count($argv) === 1) {
     echo "\n";
-    echo "  \e[0;31;41m Missing filename parameter \e[0m\n";
-    echo "   Usage: php " . $argv[0] . ' csvFile.csv';
+    echo "  \e[0;31;41m Missing filename parameter \e[0m\n\n";
+    echo "   Usage:   php " . $argv[0] . " [file name] [number of pages (optional)]\n";
+    echo "   Example: php " . $argv[0] . ' filename.csv 222';
     echo "\n";
     die;
+}
+
+if (array_key_exists(2, $argv)) {
+    $numberOfPages = $argv[2];
+} else {
+    $numberOfPages = -1;
 }
 
 $composeExporter = new ComposeExporter();
 $composeExporter->addExporter(new CsvExporter($argv[1]));
 $composeExporter->addExporter(new CliStatusExporter());
 
-$retriever->retrieve(400, 2, $composeExporter);
+$retriever->retrieve(400, $numberOfPages, $composeExporter);
